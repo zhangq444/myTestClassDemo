27f494b2e2049a7852f947c54f373d8e95df4e59
DESKTOP-CT4OS6H\grzha <grzhangqiang@163.com>
2022-08-07 11:02:37 +0800

    20220806  自测类编写

---
+++ src/test/java/com/newTouch/testClassDemo/controller/TestRollBackController.java
@@ -0,0 +1,18 @@
+package com.newTouch.testClassDemo.controller;
+
+import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+@Slf4j
+public class TestRollBackController {
+
+    @Test
+    public void test01(){
+        System.out.println("第一次提交");
+    }
+
+
+
+}
