064fd0dfac326c56ac119da1c744afc990a00e91
DESKTOP-CT4OS6H\grzha <grzhangqiang@163.com>
2022-08-07 11:12:19 +0800

    20220806  自测类编写

--- src/test/java/com/newTouch/testClassDemo/controller/TestRollBackController.java
+++ src/test/java/com/newTouch/testClassDemo/controller/TestRollBackController.java
@@ -13,6 +13,7 @@ public class TestRollBackController {
         System.out.println("第一次提交");
         System.out.println("第二次提交");
         System.out.println("第三次提交");
+        System.out.println("第四次提交");
     }
 
 
